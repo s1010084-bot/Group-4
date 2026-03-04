6c4bdc6ef7181037708ce7d4ad2aa73b3d5c4beb
CHRISTIAN JED F. GONZALES <2302278@cc.usls.edu>
2026-03-04 15:48:45 +0800

    Roomies

--- src/Rooms.java
+++ src/Rooms.java
@@ -1,2 +1,24 @@
 public class Rooms {
+    String room; // Room name
+    int roomNumber; // Room number indication (what direction it go idk)
+
+    public Rooms(){
+
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
 }
