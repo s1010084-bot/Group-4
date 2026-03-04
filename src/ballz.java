c041d3e66c4872cb2823bac0b02e72e07bf6efa4
ARES DANIEL JANAGAP <2402470@cc.usls.edu>
2026-03-04 15:37:37 +0800

    ballz

---
+++ src/ballz.java
@@ -0,0 +1,15 @@
+public class ballz {
+    private String sound;
+    public void bounce(String sound){
+        System.out.print(sound);
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+}
+
