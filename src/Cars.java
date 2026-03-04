1e0da7474ff10a0a2e6f2bd074c4c86fc2a3e3af
BarryLopezlab <barrylopez122@gmail.com>
2026-03-04 15:42:55 +0800

    Cars2

--- src/Cars.java
+++ src/Cars.java
@@ -1,3 +1,16 @@
 public class Cars {
     //BARRY
+    String cars;
+
+    public Cars(String cars) {
+        this.cars = cars;
+    }
+
+    public String getCars() {
+        return cars;
+    }
+
+    public void setCars(String cars) {
+        this.cars = cars;
+    }
 }
