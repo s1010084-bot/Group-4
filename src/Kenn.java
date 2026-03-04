c2bb9e21ecf8e225e4394380545da156c665231d
2400150 <s2400150@usls.edu.ph>
2026-03-04 15:41:11 +0800

    added a java class /Kenn

---
+++ src/Kenn.java
@@ -0,0 +1,25 @@
+public class Kenn{
+    private String fname;
+    private String lname;
+
+    public Kenn(String fname, String lname) {
+        this.fname = fname;
+        this.lname = lname;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+}
